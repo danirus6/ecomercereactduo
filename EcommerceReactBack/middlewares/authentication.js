50cbca3d42524da03903224b3ca4970b620a2c4d
AdrikLarreategui <adrian15186@hotmail.com>
2024-02-25 14:24:37 +0100

    middlewares actualizados

--- EcommerceReactBack/middlewares/authentication.js
+++ EcommerceReactBack/middlewares/authentication.js
@@ -1,55 +1,39 @@
-const { User, Token, Sequelize } = require('../models');
-
+const { User, Token, Sequelize } = require("../models");
 const { Op } = Sequelize;
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/config.json")["development"];
 
-const jwt = require('jsonwebtoken');
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const payload = jwt.verify(token, jwt_secret);
+    const user = await User.findByPk(payload.id);
+    const tokenFound = await Token.findOne({
+      where: {
+        [Op.and]: [{ UserId: user.id }, { token: token }],
+      },
+    });
+    if (!tokenFound) {
+      return res.status(401).send({ message: "You are not authorized" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ error, message: "There was a problem with the token" });
+  }
+};
 
-const {jwt_secret} = require('../config/config.json')['development']
+const isAdmin = async (req, res, next) => {
+  const admins = ["admin", "superadmin"];
+  if (!admins.includes(req.user.role)) {
+    return res.status(403).send({
+      message: "You dont have permissions",
+    });
+  }
+  next();
+};
 
-const authentication = async(req, res, next) => {
-
-try {
-
-const token = req.headers.authorization;
-
-const payload = jwt.verify(token, jwt_secret);
-
-const user = await User.findByPk(payload.id);
-
-const tokenFound = await Token.findOne({
-
-where: {
-
-[Op.and]: [
-
-{ UserId: user.id },
-
-{ token: token }
-
-]
-
-}
-
-});
-
-if (!tokenFound) {
-
-return res.status(401).send({ message: 'No estas autorizado' });
-
-}
-
-req.user = user;
-
-next();
-
-} catch (error) {
-
-console.log(error)
-
-res.status(500).send({ error, message: 'Ha habido un problema con el token' })
-
-}
-
-}
-
-module.exports = { authentication }
+module.exports = { authentication, isAdmin };
